eb03b301265aa144ff9f5924f3b74d7d6b647e81
Marvin Krüger <marvin.krueger@yahoo.de>
2017-12-18 11:42:47 +0100

    Refactored and added comments

--- Graded2/tf-idf.py
+++ Graded2/tf-idf.py
@@ -2,20 +2,26 @@ import json
 import math
 
 def search(query):
+    #Read the term frequencies
     with open("index.txt") as f:
         content = f.readlines()
 
     content = [x.strip() for x in content]
+
+    #Add the search query to tf dic
     search_query_words = query.split(" ")
     query_dic = {}
     for word in search_query_words:
         query_dic[word] = 1
     content.append("{\"search_query\":" + str(query_dic).replace("\'", "\"") + "}")
+
+    #Parse from JSON to dics
     docs = len(content)
     parsed_docs = []
     for doc in content:
         parsed_docs.append(json.loads(doc))
 
+    #Determine document frequencies for terms
     dfs = {}
     for page in parsed_docs:
         for site in page:
@@ -28,16 +34,14 @@ def search(query):
     for entry in dfs:
         dfs[entry] = math.log10(docs / dfs[entry])
 
+    #Determine the tf-idf weights for all terms
     tidfs = {}
-
     for doc in parsed_docs:
         tidf = {}
         for site_content in doc:
             for term in doc[site_content]:
                 tidf[term] = (1+ math.log10(doc[site_content][term])) * dfs[term]
             tidfs[site_content] = tidf
-
-    print(tidfs)
 
     #Get length of each tf-idf document vector
     doc_vec_length = {}
@@ -46,16 +50,14 @@ def search(query):
         for value in tidfs[doc]:
             doc_vec_length[doc] += math.pow(tidfs[doc][value], 2)
         doc_vec_length[doc] = math.sqrt(doc_vec_length[doc])
-    print(doc_vec_length)
 
-    normalized_vecs = tidfs
     #Normalize the doc_vecs
+    normalized_vecs = tidfs
     for doc in normalized_vecs:
         for value in normalized_vecs[doc]:
             normalized_vecs[doc][value] = normalized_vecs[doc][value] / doc_vec_length[doc]
 
-    print(normalized_vecs)
-    #Search in each document
+    #Search in each document for search_query queries
     search_query = tidfs.pop("search_query")
     match = {}
     for doc in normalized_vecs:
@@ -65,11 +67,13 @@ def search(query):
                 match[doc] += search_query[value] * normalized_vecs[doc][value]
     return match
 
-
+#Lets do a search for the given queries and save results in tfidf_search
 search_queries = ["tokens", "index", "classification", "tokens classification"]
 result = ""
 for query in search_queries:
     result += "{\"" + query + "\":" + str(search(query)) + "}\n"
 
+print("Results:")
+print(result)
 with open('tfidf_search.txt', 'a') as the_file:
     the_file.write(result)
